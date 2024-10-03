d051a45aa15e2adbeecb7f575e435c0d44debbe5
Kathleen Santos <kathleen.santos@csacademy.com.br>
2024-10-02 21:34:33 -0300

    feat: implementing logic for logging into the platform

--- src/components/signIn/index.tsx
+++ src/components/signIn/index.tsx
@@ -11,6 +11,7 @@ import z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
 
 const schemaSignIn = z.object({
   email: z.string().email({ message: "Insira um e-mail válido" }),
@@ -25,21 +26,46 @@ const SignIn = () => {
     resolver: zodResolver(schemaSignIn),
   });
 
-  const onSubimit = (userData: any) => {
-    const localStorangeData = localStorage.getItem("userDate");
-    if (localStorangeData) {
-      const objectUserData = JSON.parse(localStorangeData);
+  const { mutate: signIn } = useMutation({
+    mutationFn: async ({ email, password }: any) => {
+      const signInAPI = "https://66c019dbba6f27ca9a55bb0c.mockapi.io/users";
+      const response = await fetch(signInAPI);
 
-      objectUserData.email === userData.email &&
-      objectUserData.password === userData.password
-        ? navigate("/home")
-        : alert("Usuário não cadastrado");
-    }
+      const users = await response.json();
+
+      const user = users.find(
+        (user: any) => user.email === email && user.password === password
+      );
+
+      if (user) {
+        navigate("/home");
+      } else {
+        console.error("Email ou senha inválidos");
+      }
+    },
+    onSuccess: () => {},
+    onError: () => {},
+  });
+
+  const onSubimit = (userData: any) => {
+    signIn(userData);
   };
 
   return (
     <Flex mih={"100vh"} bg={"#000"} justify={"center"} align={"center"}>
-      <Flex classNames={style} direction={"column"} align={"center"} p={"36"} w={600} style={{ borderRadius: 16, position: "relative", backdropFilter: "blur(2px)", zIndex: 2 }}>
+      <Flex
+        classNames={style}
+        direction={"column"}
+        align={"center"}
+        p={"36"}
+        w={600}
+        style={{
+          borderRadius: 16,
+          position: "relative",
+          backdropFilter: "blur(2px)",
+          zIndex: 2,
+        }}
+      >
         <Image w={"80%"} src={"../../public/img/logo.png"} />
         <Flex
           component="form"
@@ -47,7 +73,7 @@ const SignIn = () => {
           gap={16}
           direction={"column"}
           onSubmit={handleSubmit(onSubimit)}
-          style={{ zIndex: 0}}
+          style={{ zIndex: 0 }}
           p={8}
         >
           <TextInput
@@ -71,23 +97,21 @@ const SignIn = () => {
             error={formState.errors.password?.message}
           />
           <Flex>
-            <Anchor c={"#05acc4"} fw={600} fz={12} onClick={() => navigate("/")}>
+            <Anchor
+              c={"#05acc4"}
+              fw={600}
+              fz={12}
+              onClick={() => navigate("/")}
+            >
               Vai, preenche esse cadastro inútil... ou não, tanto faz.
             </Anchor>
           </Flex>
           <Button type="submit">Abrir Portal</Button>
         </Flex>
         <Flex justify={"center"}>
-        <Image
-          w={"30%"}
-        
-          src={
-            "../../public/img/rick-signIn.png"
-          }
-        />
+          <Image w={"30%"} src={"../../public/img/rick-signIn.png"} />
         </Flex>
       </Flex>
-        
     </Flex>
   );
 };
