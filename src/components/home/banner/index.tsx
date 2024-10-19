7dd6fd6ae333be996a500b35d0e0c09fafa43753
Kathleen Santos <kathleen.santos@csacademy.com.br>
2024-10-19 15:13:41 -0300

    feat: ajustando código

--- src/components/home/banner/index.tsx
+++ src/components/home/banner/index.tsx
@@ -1,18 +1,29 @@
 import { Flex } from "@mantine/core";
 import imgBanner from "./banner.json";
+import '@mantine/carousel/styles.css';
+import { Carousel } from '@mantine/carousel';
 
 const Banner = () => {
 
   return (
-    <Flex
-    w={"100%"}
-    h={550}
-      style={{
-        borderRadius: 16,
-        backgroundImage: `url(${imgBanner.imagens.map((image) => (image))})`,
-        backgroundSize: "cover",
-      }}
-    ></Flex>
+    <Flex h={550}
+    w={"100%"}>
+        <Carousel
+        h={550}
+        w={"100%"}
+    >
+         {imgBanner.imagens.map((image, index) => (
+          <Carousel.Slide
+            key={index}
+            style={{
+              backgroundImage: `url(${image})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          ></Carousel.Slide>
+        ))}
+    </Carousel>
+    </Flex>
   );
 };
 
